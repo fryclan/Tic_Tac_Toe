ce1a25b2765e10a9bf8c1fde872f2e962593d198
fryclan <fry.jeffery123454@gmail.com>
2023-11-03 14:45:24 -0700

    finished the def for checking game state that will return 0 if someone has won.

---
+++ while_loop_def.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int win_condition_check(void)
+{
+    //should return 1 unless it finds a 3 in a row that isn't _ or blank.
+    char board[16] = "___\n___\n___\n";
+    while (board[0]== board[1],board[0]==board[2],board[0]!= "_")
+    {
+        while(board[4]==board[5],board[4]==board[6],board[4]!= "_")
+        {
+            while (board[8]==board[9],board[8]==board[10],board[8]!= "_")
+            {
+                while (board[0]== board[4],board[0]==board[8],board[0]!= "_")
+                {
+                    while(board[1]==board[5],board[1]==board[9],board[1]!= "_")
+                    {
+                        while (board[2]==board[6],board[2]==board[10],board[2]!= "_")
+                        {
+                            while (board[0]== board[5],board[0]==board[10],board[0]!= "_")
+                            {
+                                while(board[2]==board[5],board[1]==board[8],board[2]!= "_")
+                                { 
+                                    return 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            } 
+        }
+    }
+    return 1;
+}
+
+    
